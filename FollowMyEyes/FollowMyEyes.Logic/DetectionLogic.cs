64f8a48c40e63018bc9d6cf57400a3b27400a8ee
Serhiy Sakharuk <serhiyss9@gmail.com>
2013-03-21 11:12:43 +0200

    Fix face detection functionality
    stable version for notepad process

--- FollowMyEyes/FollowMyEyes.Logic/DetectionLogic.cs
+++ FollowMyEyes/FollowMyEyes.Logic/DetectionLogic.cs
@@ -16,13 +16,14 @@ namespace FollowMyEyes.Logic
 		public static IImage GetImageFromCamera(Capture capture, HaarCascade haarCascade)
 		{
 			Image<Bgr, byte> frame;
-			using (frame = capture.QueryFrame())
+			frame = capture.QueryFrame();
+			//using (frame = capture.QueryFrame())
 			{
 				if (frame != null)
 				{
 					Image<Gray, Byte> grayFrame = frame.Convert<Gray, Byte>();
 					MCvAvgComp[][] faces = grayFrame.DetectHaarCascade(haarCascade, 1.4, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
-																	   new Size(640, 480));
+																	   new Size(640/4, 480/3));
 
 					foreach (MCvAvgComp face in faces[0])
 						frame.Draw(face.rect, new Bgr(0, double.MaxValue, 0), 3);
@@ -39,9 +40,9 @@ namespace FollowMyEyes.Logic
 				{
 					Image<Gray, Byte> grayFrame = frame.Convert<Gray, Byte>();
 					MCvAvgComp[][] faces = grayFrame.DetectHaarCascade(haarCascade, 1.4, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
-																	   new Size(640, 480));
+																	   new Size(640/4, 480/3));
 
-					if(faces[0].Length >1)
+					if(faces[0].Length >0)
 					{
 						System.Diagnostics.Process[] p = System.Diagnostics.Process.GetProcessesByName("notepad");
 						if (p.Length > 0)
